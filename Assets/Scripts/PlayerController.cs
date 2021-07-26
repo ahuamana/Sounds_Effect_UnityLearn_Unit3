85e939892affa018aa90b3c75b06ff0633b4ae7c
Alikhan <73962071@continental.edu.pe>
2021-07-26 16:11:38 -0500

    Impulse, It's a force mode which applies inmediatly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
     {
+
+       playerRb = GetComponent<Rigidbody>();
+       
+       
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Impulse, It's a force mode which applies inmediatly
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
     }
 }
