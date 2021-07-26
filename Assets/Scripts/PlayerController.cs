4501b3f8d88cd490b5689aee74794500dca020c8
Alikhan <73962071@continental.edu.pe>
2021-07-26 16:18:59 -0500

    Manage gravity programmatly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+
     // Start is called before the first frame update
     void Start()
     {
 
        playerRb = GetComponent<Rigidbody>();
-       
-       
+
+        //change gravity of physics
+
+        Physics.gravity *= gravityModifier;
         
     }
 
@@ -21,7 +26,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Impulse, It's a force mode which applies inmediatly
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
